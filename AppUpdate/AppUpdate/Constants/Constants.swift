09c906ed0f0e68d76d9fa73faddd529d031d5e04
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-25 15:10:39 +0530

    Custom launch navigation added

---
+++ AppUpdate/AppUpdate/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  AppUpdate
+//
+//  Created by Kavya Krishna K. on 25/11/24.
+//
+
+import UIKit
+
+let kScreenSize = UIScreen.main.bounds
+let kScreenWidth = UIScreen.main.bounds.width
+let kScreenHeight = UIScreen.main.bounds.height
+
+let kAppDelegate = UIApplication.shared.delegate as! AppDelegate
+var kSceneDelegate: SceneDelegate?
+var localTimeZoneIdentifier: String { return TimeZone.current.identifier }
+
