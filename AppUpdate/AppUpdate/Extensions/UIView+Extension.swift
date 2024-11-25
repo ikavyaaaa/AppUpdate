057c4c7ab57b3709514e5cca44f126f4ee497576
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-25 15:21:57 +0530

    UIView extension added

---
+++ AppUpdate/AppUpdate/Extensions/UIView+Extension.swift
@@ -0,0 +1,376 @@
+//
+//  UIView+Extension.swift
+//  AppUpdate
+//
+//  Created by Kavya Krishna K. on 25/11/24.
+//
+
+import UIKit
+
+//@IBDesignable
+extension UIView {
+    
+    
+    
+    @IBInspectable
+    /// Should the corner be as circle
+    public var circleCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            cornerRadius = newValue ? min(bounds.size.height, bounds.size.width) / 2 : cornerRadius
+        }
+    }
+    
+    
+    @IBInspectable
+    /// Should the corner be as circle
+    public var topCorenerCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            cornerRadius = newValue ? min(bounds.size.height, bounds.size.width) / 2 : cornerRadius
+        }
+    }
+    
+    
+    @IBInspectable
+    /// Should the corner be as circle
+    public var topRightCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            let maskPath1 = UIBezierPath(roundedRect: bounds,byRoundingCorners: [.bottomLeft , .topRight],cornerRadii: CGSize(width: 10, height: 10))
+            let maskLayer1 = CAShapeLayer()
+            maskLayer1.frame = bounds
+            maskLayer1.path = maskPath1.cgPath
+            layer.mask = maskLayer1
+            return
+        }
+    }
+    
+    @IBInspectable
+    /// Should the corner be as circle
+    public var buttomLeftCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            let maskPath1 = UIBezierPath(roundedRect: bounds,byRoundingCorners: [.bottomLeft],cornerRadii: CGSize(width: 10, height: 10))
+            let maskLayer1 = CAShapeLayer()
+            maskLayer1.frame = bounds
+            maskLayer1.path = maskPath1.cgPath
+            layer.mask = maskLayer1
+            return
+        }
+    }
+    
+    
+    
+    @IBInspectable
+    /// Should the corner be as circle
+    public var topRightLeftCorner: Bool {
+        get {
+            return min(bounds.size.height, bounds.size.width) / 2 == cornerRadius
+        }
+        set {
+            let maskPath1 = UIBezierPath(roundedRect: bounds,byRoundingCorners: [.topLeft , .topRight],cornerRadii: CGSize(width: 10, height: self.height))
+            let maskLayer1 = CAShapeLayer()
+            maskLayer1.frame = bounds
+            maskLayer1.path = maskPath1.cgPath
+            layer.mask = maskLayer1
+            return
+        }
+    }
+    
+    
+    
+    
+    @IBInspectable
+    /// Corner radius of view; also inspectable from Storyboard.
+    public var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = circleCorner ? min(bounds.size.height, bounds.size.width) / 2 : newValue
+            //abs(CGFloat(Int(newValue * 100)) / 100)
+        }
+    }
+    
+    
+    
+    @IBInspectable
+    /// Border color of view; also inspectable from Storyboard.
+    public var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let color = newValue else {
+                layer.borderColor = nil
+                return
+            }
+            layer.borderColor = color.cgColor
+        }
+    }
+    
+    @IBInspectable
+    /// Border width of view; also inspectable from Storyboard.
+    public var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Shadow color of view; also inspectable from Storyboard.
+    public var shadowColor: UIColor? {
+        get {
+            guard let color = layer.shadowColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    /// Shadow offset of view; also inspectable from Storyboard.
+    public var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Shadow opacity of view; also inspectable from Storyboard.
+    public var shadowOpacity: Double {
+        get {
+            return Double(layer.shadowOpacity)
+        }
+        set {
+            layer.shadowOpacity = Float(newValue)
+        }
+    }
+    
+    @IBInspectable
+    /// Shadow radius of view; also inspectable from Storyboard.
+    public var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Shadow path of view; also inspectable from Storyboard.
+    public var shadowPath: CGPath? {
+        get {
+            return layer.shadowPath
+        }
+        set {
+            layer.shadowPath = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Should shadow rasterize of view; also inspectable from Storyboard.
+    /// cache the rendered shadow so that it doesn't need to be redrawn
+    public var shadowShouldRasterize: Bool {
+        get {
+            return layer.shouldRasterize
+        }
+        set {
+            layer.shouldRasterize = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Should shadow rasterize of view; also inspectable from Storyboard.
+    /// cache the rendered shadow so that it doesn't need to be redrawn
+    public var shadowRasterizationScale: CGFloat {
+        get {
+            return layer.rasterizationScale
+        }
+        set {
+            layer.rasterizationScale = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Corner radius of view; also inspectable from Storyboard.
+    public var maskToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+    
+    @IBInspectable
+    /// Corner radius of view; also inspectable from Storyboard.
+    public var dashedBorder: Bool {
+        //  if self.dashedBorder == true {
+        let dashBorder = CAShapeLayer()
+        dashBorder.lineWidth = 1
+        dashBorder.strokeColor = UIColor.red.cgColor
+        dashBorder.lineDashPattern = [6, 6] as [NSNumber]
+        dashBorder.frame = bounds
+        dashBorder.fillColor = nil
+        if cornerRadius > 0 {
+            dashBorder.path = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
+        } else {
+            dashBorder.path = UIBezierPath(rect: bounds).cgPath
+        }
+        dashBorder.lineJoin = CAShapeLayerLineJoin.round
+        dashBorder.name = "kShapeDashed"
+        layer.addSublayer(dashBorder)
+        return true
+        //        }else { return false }
+    }
+    func addDashedBorder() {
+        let color = #colorLiteral(red: 0.2669999897, green: 0.5609999895, blue: 1, alpha: 1)
+        let shapeLayer:CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = 2
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [4,6]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 5).cgPath
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    func animate() {
+        UIView.animate(withDuration: 0.2, animations: { self.transform = CGAffineTransform(scaleX: 0.978, y: 0.98)},completion: { finish in
+            UIView.animate(withDuration: 0.2, animations: { self.transform = CGAffineTransform.identity})
+        })
+    }
+    
+    
+    func animateFromRight() {
+        UIView.animate(withDuration: 0.35, animations: {
+            let moveLeft = CGAffineTransform(translationX: self.bounds.width, y: 0.0)
+            self.transform = moveLeft
+
+        })
+    }
+    
+    
+    @IBInspectable var isGradient: Bool{
+        get{
+            return self.isGradient
+        }
+        set (hasDone) {
+            if hasDone{
+                setGradient()
+            }
+        }
+    }
+    
+    func setGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: -0.8)
+        gradientLayer.endPoint = CGPoint(x: 0.8, y: 0.9)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        self.layoutIfNeeded()
+    }
+    
+   
+}
+
+// MARK: - Properties
+public extension UIView {
+    
+    /// Size of view.
+    var size: CGSize {
+        get {
+            return self.frame.size
+        }
+        set {
+            self.width = newValue.width
+            self.height = newValue.height
+        }
+    }
+    
+    /// Width of view.
+    var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            self.frame.size.width = newValue
+        }
+    }
+    
+    /// Height of view.
+    var height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            self.frame.size.height = newValue
+        }
+    }
+}
+
+
+extension UIView {
+
+    func applyGradient(colours: [UIColor]) -> CAGradientLayer {
+        return self.applyGradient(colours: colours, locations: nil)
+    }
+
+
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> CAGradientLayer {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+        return gradient
+    }
+    
+    func addShadow() {
+        layer.cornerRadius = 10
+        layer.borderColor = UIColor.darkGray.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 3
+        layer.shadowColor  = UIColor.darkGray.cgColor
+    }
+    
+    func applyRoundedCorners() {
+        maskToBounds = true
+        clipsToBounds = true
+        layer.cornerRadius = 30
+        layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+    
+
+    
+}
---
+++ AppUpdate/AppUpdate/MVVM/Login/LoginVC.swift
@@ -0,0 +1,165 @@
+//
+//  LoginVC.swift
+//  AppUpdate
+//
+//  Created by Kavya Krishna K. on 25/11/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet var viewHeader: UIView!
+    @IBOutlet var lblHeader: UILabel!
+    @IBOutlet var stackButtons: UIStackView!
+    @IBOutlet weak var viewMain: UIView!
+    @IBOutlet weak var btnApple: UIButton!
+    @IBOutlet weak var btnGoogle: UIButton!
+    @IBOutlet var btnLogin: TransitionButton!
+    @IBOutlet var txtFldEmail: UITextField!
+    @IBOutlet var txtFldPassword: UITextField!
+    @IBOutlet var btnForgotPassword: UIButton!
+    @IBOutlet var btnSignUp: UIButton!
+    
+    // MARK: Properties
+    private var isLayoutUpdated = Bool()
+    
+    // MARK: - View Lifecycle
+    /// Called after the view controller has loaded its view into memory.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewMain.applyRoundedCorners()
+        setupAnimation()
+    }
+    
+    /// - Parameter animated: A Boolean value indicating whether the view is being added to the window using an animation.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        
+    }
+
+    /// This method is called to notify the view controller that its view has just laid out its subviews.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if !isLayoutUpdated {
+            updateLayout()
+        }
+    }
+    
+
+    // MARK: - Layout Updates
+    
+    /// This function sets up and performs animations for various UI elements on the login screen.
+    func setupAnimation() {
+        // Set initial alpha values to 0 for the text fields, stack view, and login button to make them invisible
+        self.txtFldEmail.alpha = 0.0
+        self.txtFldPassword.alpha = 0.0
+        self.stackButtons.alpha = 0.0
+        self.btnLogin.alpha = 0.0
+        
+        // Animate the header label
+        UIView.animate(withDuration: 0.3, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the header label upwards and make it visible
+            self.lblHeader.center.y -= self.lblHeader.center.y + 10
+            self.lblHeader.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) {
+                self.lblHeader.alpha = 1.0
+            }
+        }, completion: nil)
+        self.lblHeader.isHidden = false
+        
+        // Animate the email text field
+        UIView.animate(withDuration: 0.4, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the email text field upwards and make it visible
+            self.txtFldEmail.center.y -= self.txtFldEmail.center.y + 10
+            self.txtFldEmail.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) { self.txtFldEmail.alpha = 1.0 }
+        }, completion: nil)
+        self.txtFldEmail.isHidden = false
+        
+        // Animate the password text field
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the password text field upwards and make it visible
+            self.txtFldPassword.center.y -= self.txtFldPassword.center.y + 10
+            self.txtFldPassword.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) { self.txtFldPassword.alpha = 1.0 }
+        }, completion: nil)
+        self.txtFldPassword.isHidden = false
+        
+        // Animate the stack view containing buttons
+        UIView.animate(withDuration: 0.6, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the stack view upwards and make it visible
+            self.stackButtons.center.y -= self.stackButtons.center.y + 10
+            self.stackButtons.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) { self.stackButtons.alpha = 1.0 }
+        }, completion: nil)
+        self.stackButtons.isHidden = false
+        
+        // Animate the login button
+        UIView.animate(withDuration: 0.7, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the login button upwards and make it visible
+            self.btnForgotPassword.center.y -= self.btnForgotPassword.center.y + 10
+            self.btnForgotPassword.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) { self.btnForgotPassword.alpha = 1.0 }
+        }, completion: nil)
+        self.btnForgotPassword.isHidden = false
+        
+        // Animate the login button
+        UIView.animate(withDuration: 0.7, delay: 0.1, options: [.curveLinear], animations: {
+            // Move the login button upwards and make it visible
+            self.btnLogin.center.y -= self.btnLogin.center.y + 10
+            self.btnLogin.layoutIfNeeded()
+            UIView.animate(withDuration: 0.2) { self.btnLogin.alpha = 1.0 }
+        }, completion: nil)
+        self.btnLogin.isHidden = false
+        
+        // Uncomment if you want to animate the forgot password button
+        // btnForgotPassword.animShowHidden()
+        
+        // Update the layout of the view's subviews
+        view.layoutSubviews()
+        
+        // Uncomment if you want to animate the header label's text
+        // lblHeader.animate(newText: lblHeader.text ?? "Login", characterDelay: 0.4)
+    }
+    
+    /// This function updates the layout and appearance of various UI elements.
+    func updateLayout() {
+        // Set the corner radius for the Apple button to make it circular
+        btnApple.layer.cornerRadius = btnApple.frame.width / 2
+        
+        // Set the corner radius for the Google button to match the Apple button
+        btnGoogle.layer.cornerRadius = btnApple.frame.width / 2
+        
+        // Set a horizontal gradient background color for the main view with a corner radius of 10
+        viewMain.setGradientBackgroundColor(colors: [UIColor.green, UIColor.blue], axis: .horizontal, cornerRadius: 10) { view in
+            // Ensure the view is a UIButton and bring the image view to the front
+            guard let btn = view as? UIButton, let imageView = btn.imageView else { return }
+            btn.bringSubviewToFront(imageView)
+        }
+        
+        // Set a horizontal gradient background color for the login button with a corner radius of 10
+        btnLogin.setGradientBackgroundColor(colors: [UIColor.green, UIColor.blue], axis: .horizontal, cornerRadius: 10) { view in
+            // Ensure the view is a UIButton and bring the image view to the front
+            guard let btn = view as? UIButton, let imageView = btn.imageView else { return }
+            btn.bringSubviewToFront(imageView)
+        }
+        
+        // Adjust the gradient layer for the Apple button to fit its bounds and set its corner radius
+        if let btnAppleLayer = btnApple.layer.sublayers?.first as? CAGradientLayer {
+            btnAppleLayer.frame = btnApple.bounds
+            btnAppleLayer.cornerRadius = btnAppleLayer.bounds.width / 2
+        }
+        
+        // Adjust the gradient layer for the Google button to fit its bounds and set its corner radius
+        if let btnGoogleLayer = btnGoogle.layer.sublayers?.first as? CAGradientLayer {
+            btnGoogleLayer.frame = btnGoogle.bounds
+            btnGoogleLayer.cornerRadius = btnGoogleLayer.bounds.width / 2
+        }
+        
+        isLayoutUpdated = true
+    }
+    
+
+}
