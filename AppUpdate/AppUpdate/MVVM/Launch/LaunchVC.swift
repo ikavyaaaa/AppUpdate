413685f9a2744aca9a8cb7519ec53518238a2635
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-25 15:06:54 +0530

    UIViewController extension added

--- AppUpdate/AppUpdate/MVVM/Launch/LaunchVC.swift
+++ AppUpdate/AppUpdate/MVVM/Launch/LaunchVC.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class LaunchVC: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var imgVwLogo: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.imgVwLogo.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+        UIView.animate(withDuration: 1.0, delay: 0, options: [], animations: {
+            self.imgVwLogo.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion:  nil)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
