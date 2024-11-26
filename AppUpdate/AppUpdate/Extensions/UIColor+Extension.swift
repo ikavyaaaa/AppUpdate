9450c393adf36a36697af49a53943d76bfa60592
Kavya Krishna <kavyakrishna@zktecodev.com>
2024-11-26 10:52:28 +0530

    UIColor extension added

---
+++ AppUpdate/AppUpdate/Extensions/UIColor+Extension.swift
@@ -0,0 +1,62 @@
+//
+//  UIColor+Extension.swift
+//  AppUpdate
+//
+//  Created by Kavya Krishna K. on 26/11/24.
+//
+
+import UIKit
+
+extension UIColor {
+    struct Theme {
+        static var appUpdateBlack : UIColor  { return UIColor(named: "AccentColor")! }
+        static var appUpdateBlue : UIColor  { return UIColor(named: "SecondaryGradientCOlor")! }
+        static var appUpdateGreen : UIColor  { return UIColor(named: "PrimaryGradientColor")! }
+        static var appUpdateWhite : UIColor  { return UIColor(named: "PrimaryColor")! }
+    }
+    
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        //assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+}
+
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
+        }
+        
+        if hexString.count != 6 {
+            self.init(white: 0.5, alpha: 1.0)
+            return
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: 1.0
+        )
+    }
+}
+
